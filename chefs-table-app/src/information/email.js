a83e94f209c71d1fac3c815d0ca9f17af5f10218
jgartsu12 <jgartsu12@gmail.com>
2020-02-25 11:20:30 -0500

    created email

---
+++ chefs-table-app/src/information/email.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import * as actions from '../store/actions';
+import PageTitle from '../components/pageTitle';
+import EmailForm from './emailForm';
+
+class Email extends Component {
+    componentDidMount() {
+        this.props.setHeaderLinks([]);
+        this.props.setNavbarLinks([]);
+    }
+
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
+
+    render() {
+        return (
+            <div className='email'>
+                <PageTitle className='email__page-title' title='E-mail' />
+                <EmailForm onSubmit={this.onSubmit} className='email__form' />
+            </div>
+        )
+    }
+}
+
+Email = connect(null, actions)(Email)
+
+export default Email;
